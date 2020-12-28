08e2f7ce97a8dd620e63b3795091cbfcf8da1781
utkryuk <utkryuk@gmail.com>
2020-12-28 19:46:39 +0530

    completed anecdote 1.14

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,23 +1,73 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
+// const Best = ({votes, anecdotes}) => {
+
+//   let index = 0;
+//   let best_anecdote = votes[0]
+
+//   for(let i=0; i<votes.length; i++){
+//     if(votes[i] > best_anecdote){
+//       best_anecdote = votes[i];
+//       index = i;
+//     }
+//   }
+
+//   best_anecdote = anecdotes[index];
+//   console.log(best_anecdote)
+
+//   return (
+//     <div>
+//       <h1>Anecdote with most votes</h1>
+//       {best_anecdote}<br />
+//     </div>
+//   )
+// }
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick = {handleClick}>{text}</button>
+  )
+}
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array.apply(null, {length: anecdotes.length}).map(function() {return 0;}))
+  // const [votes, setVotes] = useState(new Uint8Array(6));
+  const [mostVotes, setMostVotes] = useState(0)
+
+  
 
   const next_anecdote = () => {
     setSelected(Math.floor(Math.random()*anecdotes.length))
   }
 
+  const vote_anecdote = () => {
+    const copy = {...votes }
+    copy[selected] += 1
+    setVotes(copy)
+    // console.log(copy)
+    if (copy[selected] > copy[mostVotes]){
+      setMostVotes(selected)
+    }
+  }
+
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <button onClick = {next_anecdote}>next anecdote</button>
+      <h1>Anecdote of the Day</h1>
+      {props.anecdotes[selected]}<br />
+      has {votes[selected]} votes<br />
+      <Button handleClick = {vote_anecdote} text ="vote" />
+      <Button handleClick = {next_anecdote} text = "next anecdote" />
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[mostVotes]}<br />
+      has {votes[mostVotes]} votes<br />
+      
     </div>
   )
 }
 
   const anecdotes = [
-    'If it hurts, do it more often',
+    'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',

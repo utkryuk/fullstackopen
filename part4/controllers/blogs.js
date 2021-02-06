ce7af3e94db056b3526530ee531736ebf475b43e
utkryuk <utkryuk@gmail.com>
2021-02-06 16:04:54 +0530

    completed 4.9, 4.10 POST /api/blog tested

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -7,7 +7,7 @@ blogsRouter.get('/', async (request, response) => {
     return response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
 
     const body = request.body
     const blog = new Blog({
@@ -17,13 +17,22 @@ blogsRouter.post('/', (request, response) => {
         likes: body.likes
     })
 
-    blog.save()
-        .then(savedBlog => {
-            return response.json(savedBlog)
-        })
-        .catch(error => {
-            console.log('error', error.message)
-        })
+    // blog.save()
+    //     .then(savedBlog => {
+    //         return response.json(savedBlog)
+    //     })
+    //     .catch(error => {
+    //         console.log('error', error.message)
+    //     })
+
+    try {
+        const savedBlog = await blog.save()
+        response.json(savedBlog)    
+    }
+    catch (exception) {
+        next(exception)
+    }
+    
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -28,9 +28,44 @@ describe('Note Api Testing', () => {
 
         expect(response.body).toHaveLength(helper.initialBlogs.length)
     })
+    
+    test.only('POST request to /api/blogs', async () => {
+
+        const newBlog = {
+            title: 'Hello from testing',
+            author: 'Utkarsh Gupta',
+            url: 'hello.com',
+            likes: 69
+        }
+
+        const savedBlog = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const allBlogs = await api
+            .get('/api/blogs')
+
+        expect(allBlogs.body).toHaveLength(helper.initialBlogs.length + 1)
+
+        expect(savedBlog.body.title).toBe('Hello from testing')
+        expect(savedBlog.body.author).toBe('Utkarsh Gupta')
+        expect(savedBlog.body.url).toBe('hello.com')
+        expect(savedBlog.body.likes).toBe(69)
+    })
 
 })
 
+test('Checking that unique property is named id', async() => {
+
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    expect(response.body[0].id).toBeDefined()
+})
 
 afterAll (() => {
     mongoose.connection.close()

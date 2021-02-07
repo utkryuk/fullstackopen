42d1450eb50c1eb523da8debbcbff4d3928246d6
utkryuk <utkryuk@gmail.com>
2021-02-08 00:33:39 +0530

    completed 4.18 completed: added user authentication with jwt

---
+++ part4/controllers/login.js
@@ -0,0 +1,30 @@
+const loginRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+
+    const body = request.body
+    const user = await User.findOne({username: body.username})
+
+    const passwordCorrect = (user === null) ? false : await bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(passwordCorrect)) {
+        return response.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
+
+    const userInfoToBeTokenize = {
+        username: body.username,
+        id: body._id
+    }
+
+    const token = jwt.sign(userInfoToBeTokenize, process.env.SECRET)
+
+    return response.send({token, username: user.username, name: user.name})
+
+})
+
+module.exports = loginRouter

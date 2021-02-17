1aaeb2e17bbdb8da29e5e64adbc4a2e3e46e451e
Utkarsh Gupta <utkryuk@gmail.com>
2021-02-17 21:22:02 +0530

    completed 4.6 & 4.7: using lodash

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,6 +1,10 @@
 const dummy = require('../utils/list_helper').dummy
 const totalLikes = require('../utils/list_helper').totalLikes
 const favouriteBlog = require('../utils/list_helper').favouriteBlog
+const mostBlogs = require('../utils/list_helper').mostBlogs
+const mostLikes = require('../utils/list_helper').mostLikes
+
+const helper = require('./test_helper')
 
 test('dummy returns one', () => {
     const blogs = []
@@ -9,11 +13,6 @@ test('dummy returns one', () => {
     expect(result).toBe(1)
 })
 
-
-const blogs = [
-    { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 }, { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0 }, { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0 }, { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0 }, { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0 }, { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2, __v: 0 }    
-
-]
 
 describe('total likes', () => {
 
@@ -41,7 +40,7 @@ describe('total likes', () => {
 
     test('of a bigger list is calculated right', () => {
 
-        expect(totalLikes(blogs)).toBe(36)
+        expect(totalLikes(helper.initialBlogs)).toBe(36)
     })
 })
 
@@ -58,8 +57,60 @@ const favouriteblog = {
 describe('favourite blog', () => {
 
     test('from a bigger list', () => {
-        expect(favouriteBlog(blogs)).toEqual(favouriteblog)
+        expect(favouriteBlog(helper.initialBlogs)).toEqual(favouriteblog)
     })
 })
 
+describe('most blogs', () => {
+
+    test('from an empty list', () => {
+        
+        expect(mostBlogs([])).toEqual(null)
+    })
+    
+    test('from a single blog', () => {
+        
+        const singleBlog = [helper.initialBlogs[0]]
+
+        expect(mostBlogs(singleBlog)).toEqual({
+            author: 'Michael Chan',
+            blogs: 1
+        })
+    })
+
+    test('from a bigger list', () => {
+
+        expect(mostBlogs(helper.initialBlogs)).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+    
+})
+
+describe('most likes', () => {
+
+    test('from an empty list', () => {
+        
+        expect(mostLikes([])).toEqual(null)
+    })
+    
+    test('from a single blog', () => {
+
+        const singleBlog = [helper.initialBlogs[0]]
+
+        expect(mostLikes(singleBlog)).toEqual({
+            author: 'Michael Chan',
+            likes: 7
+        })
+    })
+
+    test('from a bigger list', () => {
+        
+        expect(mostLikes(helper.initialBlogs)).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 17
+        })
+    })
+})
 
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = () => {
     return 1
 }
@@ -24,8 +26,67 @@ const favouriteBlog = (array) => {
     })
 }
 
+const mostBlogs = (blogs) => {
+
+    if (blogs.length === 0 || blogs === null) {
+        return null
+    }
+    const groupByAuthor = lodash
+        .groupBy(blogs, (blog) => {
+            return blog.author
+        })
+
+    const authors = lodash.toArray(groupByAuthor)
+
+    // console.log(typeof(groupByAuthor))
+    // console.log(typeof(author))
+
+    const maxGroup =  lodash.maxBy(authors, (author) => {
+        return author.length
+    })
+
+    return {
+        author: maxGroup[0].author ,
+        blogs: maxGroup.length
+    }
+}
+
+const mostLikes = (blogs) => {
+
+    if (blogs === null || blogs.length === 0) {
+        return null
+    }
+
+    const groupByAuthor = lodash.groupBy(blogs, (blog) => {
+        return blog.author
+    })
+
+    const authors = lodash.toArray(groupByAuthor)
+    // console.log(authors)
+    const likes = []
+
+    for (const author of authors) {
+        // console.log(author)
+        const sum = author.reduce((sum, author) => {
+            return sum + author.likes 
+        }, 0)
+
+        likes.push({
+            author: author[0].author, 
+            likes: sum
+        })
+    }
+
+    return lodash.maxBy(likes, (obj) => {
+        return obj.likes
+    })
+
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs,
+    mostLikes
 }
